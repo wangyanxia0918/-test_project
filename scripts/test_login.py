9ca36673d71e374ca3f672ea7f0cef61971f9bd8
wangyanxia918 <wangyanxia_0918@163.com>
2019-05-31 16:19:10 +0800

    first commit

---
+++ scripts/test_login.py
@@ -0,0 +1,22 @@
+class TestLogin:
+
+    def test_login01(self):
+        print("wang")
+        assert 1
+
+    def test_login02(self):
+        print("yan")
+        assert 1
+
+    def test_login03(self):
+        print("xia")
+        assert 0
+
+    def test_login04(self):
+        print("09")
+        assert 1
+
+    def test_login05(self):
+        print("18")
+        assert 0
+
